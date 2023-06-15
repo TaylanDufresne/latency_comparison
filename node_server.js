c8d88e5310d8fe70cf52ac32b06dec7662407930
TaylanDufresne <taylan.dufresne@gmail.com>
2023-06-15 12:23:24 -0600

    first commit

---
+++ node_server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const fs = require('fs')
+
+const hostname = '127.0.0.1';
+const port = 3015;
+
+const app = express()
+
+
+app.use(express.static('public'));
+
+app.get("/button", async (req, res) => {
+    res.writeHead(200, { 'content-type': 'text/html' })
+  fs.createReadStream('loading_comparison.html').pipe(res)
+})
+
+app.get("/img", async (req, res) => {
+    res.writeHead(200, { 'content-type': 'text/html' })
+  fs.createReadStream('capture_comparison.html').pipe(res)
+})
+
+app.get("/largeimg", async (req, res) => {
+    res.writeHead(200, { 'content-type': 'text/html' })
+  fs.createReadStream('large_comparison.html').pipe(res)
+})
+
+app.get("/medimg", async (req, res) => {
+    res.writeHead(200, { 'content-type': 'text/html' })
+  fs.createReadStream('1080_comparison.html').pipe(res)
+})
+
+app.listen(port)
+console.log(`Server running at http://${hostname}:${port}/`);
